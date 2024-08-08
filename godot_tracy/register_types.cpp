e18748f954f6920857a4eea827b1400f3411e76d
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-08-08 16:38:37 +0900

    feat: apply changes of godot 4.2

--- godot_tracy/register_types.cpp
+++ godot_tracy/register_types.cpp
@@ -1,7 +1,21 @@
 #include "register_types.h"
 
-void register_godot_tracy_types() {
+// void register_godot_tracy_types() {
+// }
+
+// void unregister_godot_tracy_types() {
+// }
+
+#include "core/object/class_db.h"
+
+void initialize_godot_tracy_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 }
 
-void unregister_godot_tracy_types() {
+void uninitialize_godot_tracy_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
 }
